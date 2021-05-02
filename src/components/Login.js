6bac13110dd4b73f459b6b1d600022f034717ead
Galileo <Galileo-dev@users.noreply.github.com>
2021-05-02 18:20:31 +0100

    login page

---
+++ src/components/Login.js
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>Get All Here</SignUp>
+          <Description>
+            {" "}
+            As of 02/05/2021 Axel is Unofficially not Open for Business as this
+            is not a Real Streaming Service and is just a Movie Explorer. so for
+            just €1 you can get Nothing and I mean Nothing we don't Even Take
+            Payments. But we do take Donations Which Should be Somewhere on my
+            Github Page. Did I Mention this is in No Way Affiliated with Any
+            Brand.
+          </Description>
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.section`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 80px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("/Images/MovieGrid.png");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 550px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 85%;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5em;
+  letter-spacing: 1.5px;
+`;
+
+// const mapStateToProps = (state) => ({});
+
+// const mapDispatchToProps = {};
+
+export default Login;
